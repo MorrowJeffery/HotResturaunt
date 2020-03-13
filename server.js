f4c55f586a22a951f954de4127e6b91408d41ca3
Jeff Morrow <morrowjefferya@gmail.com>
2020-03-12 21:12:25 -0700

    adjusted .post for adding reservations

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get("/", function(req, res) {
 });
 
 //this is a route that will send the user to the make a reservation page
-app.get("/add", function(req, res) {
+app.get("/api/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
@@ -80,16 +80,19 @@ app.post("/api/reserve", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
   var newReservation = req.body;
-
+  var resAvailible = true;
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
   newReservation.uniqueID = IDRef;
   IDRef++;
 
-  //console.log(newReservation);
-
+  console.log(newReservation);
   resturauntGuests.push(newReservation);
 
+  if (resturauntGuests.length > 5) {
+      resAvailible = false;
+  }
+  res.json(resAvailible);
 });
 
 // Starts the server to begin listening
